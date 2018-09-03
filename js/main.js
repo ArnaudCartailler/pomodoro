34cbdebab82758f85134d906aaac6b9036de0634
arnaudcartailler <arnaud.cartailler@gmail.com>
2018-09-03 16:16:38 +0200

    working on break and beginning

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
 
 
-var Tbegin = "";
-document.getElementById("begin").innerHTML = Tbegin;
-
 
 
 
  var d = "";
- var time = "5";
+ var time = 5;
  function go() {
    document.getElementById("start").innerHTML = time;
    time --;
@@ -17,3 +14,33 @@ document.getElementById("begin").innerHTML = Tbegin;
    }
     d = setTimeout(go,1000);
  }
+
+var time2 = 5 ;
+$(document).ready(function(){
+    $(".plus").on("click", function(){
+        document.getElementById("begin").innerHTML = time2;
+          time2++;
+    });
+});
+
+$(document).ready(function(){
+    $(".minus").on("click", function(){
+        document.getElementById("begin").innerHTML = time2;
+          time2--;
+    });
+});
+
+var time3 = 3;
+$(document).ready(function(){
+    $(".plus1").on("click", function(){
+        document.getElementById("time1").innerHTML = time3;
+          time3++;
+    });
+});
+
+$(document).ready(function(){
+    $(".minus1").on("click", function(){
+        document.getElementById("time1").innerHTML = time3;
+          time3--;
+    });
+});

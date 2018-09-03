7dd36d9ab5059aa11dc166454e434ebe3bf93171
arnaudcartailler <arnaud.cartailler@gmail.com>
2018-09-03 15:36:14 +0200

    working on deduct

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
-$(document).ready(function(){
-  $(".plus").on("click", function(){
-    $(this)
-  })
-}
+
+
+var Tbegin = "";
+document.getElementById("begin").innerHTML = Tbegin;
+
+
+
+
+ var d = "";
+ var time = "5";
+ function go() {
+   document.getElementById("start").innerHTML = time;
+   time --;
+   if (time <= 0){
+     alert("Time's up !");
+        return;
+   }
+    d = setTimeout(go,1000);
+ }

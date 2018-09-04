5450a1ea6bbc707a9dc66527b26a9f7464fefd51
arnaudcartailler <arnaud.cartailler@gmail.com>
2018-09-04 11:34:28 +0200

    working on pause button

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-
-
-
-
-
  var d = "";
  var time = 5;
+
+ //begin the deduct
  function go() {
    document.getElementById("start").innerHTML = time;
    time --;
    if (time <= 0){
-     alert("Time's up !");
+     document.getElementById("over").innerHTML = "Time's up!";
+     document.getElementById("start").style.visibility = "hidden";
         return;
    }
     d = setTimeout(go,1000);
  }
 
+
+//add time
 var time = "" ;
 $(document).ready(function(){
     $(".plus").on("click", function(){
@@ -23,6 +23,7 @@ $(document).ready(function(){
     });
 });
 
+//supp time
 $(document).ready(function(){
     $(".minus").on("click", function(){
         document.getElementById("begin").innerHTML = time2;
@@ -43,4 +44,11 @@ $(document).ready(function(){
         document.getElementById("time1").innerHTML = time3;
           time3--;
     });
+});
+
+//pause
+$(document).ready(function(){
+  $("#pause").on("click", function(){
+    $("#begin").stop();
+  });
 });
